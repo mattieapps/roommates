2c7e16d313d106ad6cf2b43ef5fe40d28a246d79
Andrew Mattie <admin@mattieapps.com>
2015-02-12 00:01:25 -0500

    Prepping for v1.3

--- roommates-android/app/src/main/java/com/mattieapps/roommates/MainActivity.java
+++ roommates-android/app/src/main/java/com/mattieapps/roommates/MainActivity.java
@@ -1,23 +1,39 @@
 package com.mattieapps.roommates;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.mattieapps.roommates.fragments.BitcoinFragment;
+import com.mattieapps.roommates.fragments.CurrencyConversionFragment;
+import com.mattieapps.roommates.fragments.RentCalFragment;
+import com.mattieapps.roommates.fragments.TipCalFragment;
+import com.mattieapps.roommates.systems.BaseActivity;
+import com.mattieapps.roommates.systems.DrawerListAdapter;
+import com.nispok.snackbar.Snackbar;
+import com.nispok.snackbar.SnackbarManager;
+import com.nispok.snackbar.listeners.ActionClickListener;
+import com.parse.ParseInstallation;
+import com.parse.ParsePush;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends BaseActivity {
 
     private String[] mItemTitles;
     private DrawerLayout mDrawerLayout;
@@ -27,40 +43,102 @@ public class MainActivity extends ActionBarActivity {
     private ActionBarDrawerToggle mDrawerToggle;
     private Toolbar mToolbar;
 
-    private RentCalFragment homeFragment;
-    private TipCalFragment aboutFragment;
+    private RentCalFragment rentCalFragment;
+    private TipCalFragment tipCalFragment;
+    private CurrencyConversionFragment currencyConversionFragment;
+    private BitcoinFragment bitcoinFragment;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
 
-    private String[] nav_drawer_items = null;
-    private int[] nav_drawer_icons = null;
+    private String[] nav_drawer_items;
+    private int[] nav_drawer_icons;
 
     private RelativeLayout drawer_list_item;
+
+    int isFragmentNumber = 0;
+
+    int rentPriceBackup;
+    int numbOfPeopleBackup;
+    String rentOutputBackup;
+    int checkAmountBackup;
+    int gratuityBackup;
+    String tipOutputBackup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean wantsUpdates = sharedPreferences.getBoolean("receiveNotifications", true);
+
+        ParseInstallation.getCurrentInstallation().saveInBackground();
+
+        if (wantsUpdates) {
+            ParsePush.subscribeInBackground("wantsUpdates");
+        } else {
+            ParsePush.unsubscribeInBackground("wantsUpdates");
+        }
+
         nav_drawer_items = new String[] {
                 "Rent Calculator",
                 "Tip Calculator",
-               // "Settings"
+                "BitCoin Prices",
+              //  "Currency Conversion",
+                "Settings"
         };
 
         nav_drawer_icons = new int[] {
                 R.drawable.ic_home,
                 R.drawable.ic_calculator,
-               // R.drawable.ic_user
+                R.drawable.ic_action_bitcoin,
+               // 0,
+                R.drawable.ic_action_settings
         };
 
-        homeFragment = new RentCalFragment();
-        aboutFragment = new TipCalFragment();
+        rentCalFragment = new RentCalFragment();
+        tipCalFragment = new TipCalFragment();
+        currencyConversionFragment = new CurrencyConversionFragment();
+        bitcoinFragment = new BitcoinFragment();
 
-        fragmentManager = getSupportFragmentManager();
-        fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.content_frame, homeFragment);
-        fragmentTransaction.commit();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        String startFragment = preferences.getString("customHomeScreen", "Rent");
+
+        if (startFragment.equals("Rent")) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, rentCalFragment);
+            fragmentTransaction.commit();
+
+            isFragmentNumber = 0;
+        }
+
+        if (startFragment.equals("Tip")) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, tipCalFragment);
+            fragmentTransaction.commit();
+
+            isFragmentNumber = 1;
+        }
+
+        if (startFragment.equals("Currency")) {
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, currencyConversionFragment);
+            fragmentTransaction.commit();
+
+            isFragmentNumber = 2;
+        }
+
+        if (startFragment.equals("BitCoin")){
+            fragmentManager = getSupportFragmentManager();
+            fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.content_frame, bitcoinFragment);
+            fragmentTransaction.commit();
+
+            isFragmentNumber = 3;
+        }
 
         //Begin Main Nav Drawer Code
 
@@ -138,6 +216,75 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         // Handle your other action bar items...
+        int id = item.getItemId();
+        if (isFragmentNumber == 0) {
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_reset) {
+
+                EditText rentprice = (EditText) findViewById(R.id.rentPriceText);
+                EditText numbpeople = (EditText) findViewById(R.id.peopleAmountText);
+                TextView output = (TextView) findViewById(R.id.outputTextView);
+
+                rentPriceBackup = Integer.valueOf(rentprice.getText().toString());
+                numbOfPeopleBackup = Integer.valueOf(numbpeople.getText().toString());
+                rentOutputBackup = output.getText().toString();
+                SnackbarManager.show(
+                Snackbar.with(getApplicationContext()) // context
+                        .text("Rent calculations cleared") // text to display
+                        .actionLabel("UNDO")
+                        .actionListener(new ActionClickListener() {
+                            @Override
+                            public void onActionClicked(Snackbar snackbar) {
+                                EditText rentprice = (EditText) findViewById(R.id.rentPriceText);
+                                EditText numbpeople = (EditText) findViewById(R.id.peopleAmountText);
+                                TextView output = (TextView) findViewById(R.id.outputTextView);
+
+                                rentprice.setText(String.valueOf(rentPriceBackup));
+                                numbpeople.setText(String.valueOf(numbOfPeopleBackup));
+                                output.setText(rentOutputBackup);
+                            }
+                        }), this);  // activity where it is displayed
+
+//                Toast.makeText(getApplicationContext(), "Numbs: " + rentPriceBackup + numbOfPeopleBackup + rentOutputBackup, Toast.LENGTH_SHORT).show();
+
+                rentprice.setText("0");
+                numbpeople.setText("0");
+                output.setText("Output:");
+            }
+        }
+        else if (isFragmentNumber == 1) {
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_reset) {
+                EditText price = (EditText) findViewById(R.id.priceEditText);
+                EditText gratuity = (EditText) findViewById(R.id.gratuityEditText);
+                TextView output = (TextView) findViewById(R.id.outputTipsTextView);
+
+                checkAmountBackup = Integer.valueOf(price.getText().toString());
+                gratuityBackup = Integer.valueOf(gratuity.getText().toString());
+                tipOutputBackup = output.getText().toString();
+
+                SnackbarManager.show(
+                Snackbar.with(getApplicationContext()) // context
+                        .text("Tip calculations cleared")
+                        .actionLabel("UNDO")
+                        .actionListener(new ActionClickListener() {
+                            @Override
+                            public void onActionClicked(Snackbar snackbar) {
+                                EditText price = (EditText) findViewById(R.id.priceEditText);
+                                EditText gratuity = (EditText) findViewById(R.id.gratuityEditText);
+                                TextView output = (TextView) findViewById(R.id.outputTipsTextView);
+
+                                price.setText(String.valueOf(checkAmountBackup));
+                                gratuity.setText(String.valueOf(gratuityBackup));
+                                output.setText(tipOutputBackup);
+                            }
+                        }), this); // activity where it is displayed
+
+                price.setText("0");
+                gratuity.setText("0");
+                output.setText("Tip Amount:");
+            }
+        }
 
         return super.onOptionsItemSelected(item);
     }
@@ -168,15 +315,26 @@ public class MainActivity extends ActionBarActivity {
 
             switch (position){
                 case 0:
-                    fragmentTransaction.replace(R.id.content_frame, homeFragment);
+                    fragmentTransaction.replace(R.id.content_frame, rentCalFragment);
                     fragmentTransaction.commit();
+
+                    isFragmentNumber = 0;
                     break;
                 case 1:
-                    fragmentTransaction.replace(R.id.content_frame, aboutFragment);
+                    fragmentTransaction.replace(R.id.content_frame, tipCalFragment);
                     fragmentTransaction.commit();
+
+                    isFragmentNumber = 1;
                     break;
                 case 2:
-                    //TODO: Settings intent
+                    fragmentTransaction.replace(R.id.content_frame, bitcoinFragment);
+                    fragmentTransaction.commit();
+
+                    isFragmentNumber = 2;
+                    break;
+                case 3:
+                    Intent intent = new Intent(getApplication(), Settings.class);
+                    startActivity(intent);
                     break;
 
                 default: position = 0;
